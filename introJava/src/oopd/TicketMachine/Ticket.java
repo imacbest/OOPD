0af0bf6981b9216058e4a1f6394df2f3e43287a3
Thomas <thomas.erin999@gmail.com>
2015-02-11 16:13:05 +0100

    created tTicketMachine & Ticket class

---
+++ introJava/src/oopd/TicketMachine/Ticket.java
@@ -0,0 +1,9 @@
+package oopd.TicketMachine;
+
+public class Ticket {
+	private float ticketPrice;
+	private String movieTitle;
+	private int totalTickets = 0;
+	private int maxTickets = 10;
+
+}
---
+++ introJava/src/oopd/TicketMachine/TicketMachine.java
@@ -0,0 +1,13 @@
+package oopd.TicketMachine;
+
+public class TicketMachine {
+	
+	private float totalReceived;
+	private float totalCash;
+	
+	public TicketMachine(){
+		
+		
+	}
+
+}
