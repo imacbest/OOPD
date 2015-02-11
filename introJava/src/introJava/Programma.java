e8afcc4b37a55d8fcd04267c4893ca97910f0517
Thomas <thomas.erin999@gmail.com>
2015-02-11 16:52:56 +0100

    c

--- introJava/src/introJava/Programma.java
+++ introJava/src/introJava/Programma.java
@@ -16,7 +16,6 @@ public class Programma {
 		System.out.println("Student naam: " + s1.getNaam());
 		System.out.println("Student nummer: " + s1.getNummer());
 		System.out.println("School naam: " + s1.getSchool().getNaam());
-		System.out.println("Done");
 	}
 
 }
