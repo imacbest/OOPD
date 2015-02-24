a4ccf53a58bdd98c7fd101594ce3190a58e180d3
maxgroen <maxgroenendijk@outlook.com>
2015-02-24 15:17:34 +0100

    test

---
+++ introJava/src/oopd/LottoApp/Lottobal.java
@@ -0,0 +1,19 @@
+package oopd.LottoApp;
+
+public class Lottobal {
+
+	private int balNummer;
+	
+	public Lottobal(int nummer) {
+		balNummer = nummer;
+	}
+	
+	public boolean isNummerGroterDan(Lottobal andereBal) {
+		return balNummer > andereBal.balNummer;
+	}
+	
+	public String toString() {
+		return "" + balNummer;
+	}
+}
+
