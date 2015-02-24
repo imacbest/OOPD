815fccb019522520c3f3fb782b5a68de4debc6ba
maxgroen <maxgroenendijk@outlook.com>
2015-02-24 15:29:33 +0100

    1

--- introJava/src/oopd/LottoApp/Glazenbol.java
+++ introJava/src/oopd/LottoApp/Glazenbol.java
@@ -1,10 +1,19 @@
 package oopd.LottoApp;
 
+import java.util.ArrayList;
+
 public class Glazenbol {
 
-	public static void main(String[] args) {
-		private int ArrayList<Lottobal> = new ArrayList<Lottobal> 
-
+	ArrayList <Lottobal> ballen = new ArrayList<Lottobal>(); 
+		
+	public void verzamelAlleBallen() {
+		for(int i = 0;i<44;i++) {
+			ballen.add(i+1, null);
+		}
+	}
+	
+	public void schepBal() {
+		
 	}
 
 }
