94c36587fc31dfb84b4efd4f8f590e9ef45d277d
maxgroen <maxgroenendijk@outlook.com>
2015-02-24 15:09:57 +0100

    test

--- introJava/src/oopd/LottoApp/LottoTestApp.java
+++ introJava/src/oopd/LottoApp/LottoTestApp.java
@@ -3,7 +3,7 @@ package oopd.LottoApp;
 public class LottoTestApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stubsdf
+		// TODO Auto-generated method stubsdft
 
 	}
 
