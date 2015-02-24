111ce4dfea78b215c1d2195112da81494d93b0d4
imacbest <thomas.erin999@gmail.com>
2015-02-24 15:33:50 +0100

    added stuff

--- introJava/src/oopd/LottoApp/Lottomachine.java
+++ introJava/src/oopd/LottoApp/Lottomachine.java
@@ -3,6 +3,7 @@ package oopd.LottoApp;
 public class Lottomachine {
 	private Scorebord scorebord;
 	private Glazenbol glazenbol;
+	
 	private static final int AANTAL_BALLEN = 6;
 	
 	public Lottomachine(){
@@ -14,9 +15,11 @@ public class Lottomachine {
 		this.glazenbol.verzamelAlleBallen();
 		this.scorebord.maakLeeg();
 		for(int i = 0; i < AANTAL_BALLEN; i++){
-			
+			this.scorebord.plaatsBal(this.glazenbol.schepBal());
 		}
-		
+		this.scorebord.plaatsBonusBal(this.glazenbol.schepBal());
+		this.scorebord.sorteerBallen();
+		this.scorebord.printScorebord();
 		
 	}
 }
