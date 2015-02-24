ef79cdb8f5ac493da0994e81167de695064beb07
imacbest <thomas.erin999@gmail.com>
2015-02-24 15:16:57 +0100

    Added lottomachine

---
+++ introJava/src/oopd/LottoApp/Lottomachine.java
@@ -0,0 +1,14 @@
+package oopd.LottoApp;
+
+public class Lottomachine {
+	private Scorebord scorebord;
+	private Glazenbol glazenbol;
+	
+	public Lottomachine Lottomachine(){
+		
+	}
+	
+	public void voerTrekkingUit(){
+		
+	}
+}
